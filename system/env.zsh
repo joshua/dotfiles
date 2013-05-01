7cc8b257cbefe01c08e9bd3e603017c1a7292787
Joshua Suggs <joshsuggs@gmail.com>
2013-05-01 11:25:05 -0400

    Added a whole lot of files and scripts.

---
+++ system/env.zsh
@@ -0,0 +1,5 @@
+export CC=/usr/local/bin/gcc-4.2
+export CPPFLAGS=-I/opt/X11/include
+
+export VISUAL='subl -n'
+export EDITOR='subl -n'
---
+++ system/path.zsh
@@ -0,0 +1,5 @@
+export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:/usr/local/share/npm/bin:$HOME/Library/Python/2.7/bin:$PATH
+
+export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export PYTHONPATH="/usr/local/lib/python2.7/site-packages:$PYTHONPATH"
