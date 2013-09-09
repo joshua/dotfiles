6a22f1596079e3c838afaac4f19dc6fd898fe6a7
Joshua Suggs <joshsuggs@gmail.com>
2013-09-09 07:53:13 -0400

    Moved ruby prompt to separate file

--- zsh/rvm.zsh
+++ zsh/rvm.zsh
@@ -2,11 +2,3 @@
 
 # Load RVM into the shell
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-
-# Add current ruby to shell right prompt
-local rvm=''
-if which rvm-prompt &> /dev/null; then
-  rvm='%{$fg[red]%}‹$(rvm-prompt i v g)›%{$reset_color%}'
-fi
-RPROMPT="${rvm}"
-unset rvm
