8841fa1bab4936467681d53db454693cd0130874
Joshua Suggs <joshsuggs@gmail.com>
2014-04-27 11:38:02 -0400

    ST3

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,7 +7,7 @@ alias ohmyzsh="st ~/.oh-my-zsh"
 alias redis-start="redis-server /usr/local/etc/redis.conf"
 alias memcache-start="/usr/local/opt/memcached/bin/memcached"
 
-local _subl=/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl
+local _subl=/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl
 alias st='${_subl} -n'
 alias stt='st .'
 
