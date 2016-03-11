2ad228e80845cae7b87329d0fa05775c11d8cb86
joshs <josh@acst.com>
2016-03-11 11:25:49 -0500

    add directory alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -15,3 +15,14 @@ alias c='clear'
 alias cll='clear && ll'
 alias cla='clear && la'
 alias flushdns='sudo dscacheutil -flushcache' # OSX ipconfig /flushdns
+
+## Directories
+hash -d src=~/src
+hash -d vnd=~src/vendor
+hash -d legolab=~src/ACS/Lego/_lab
+
+hash -d pg=$GOPATH/src/p.go
+hash -d golab=$GOPATH/src/code.acs.tech/joshs/golab
+
+hash -d corp=/Volumes/ACS/Corporate
+hash -d xfer=/Volumes/ACS/Corporate/Transfer\ Location
