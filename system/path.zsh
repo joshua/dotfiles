ee1cf676efa8f4c049f5e5253ac58aa6a6e3eab9
joshs <josh@acst.com>
2016-03-11 11:26:13 -0500

    update path for go and Postgres.app

--- system/path.zsh
+++ system/path.zsh
@@ -3,3 +3,8 @@ export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:/usr/local/sh
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
 export PYTHONPATH="/usr/local/lib/python2.7/site-packages:$PYTHONPATH"
+
+export GOPATH=$HOME/src/go
+export PATH=$PATH:/opt/boxen/homebrew/opt/go/libexec/bin:$GOPATH/bin
+
+export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.4/bin
