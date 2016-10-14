dabcd753455d860002cc9aeddae192e9224ee73b
joshs <josh@acst.com>
2016-10-14 09:31:27 -0400

    removing powershell. see powershell branch

--- powershell/prompt.ps1
+++
@@ -1,5 +0,0 @@
-# Load GitHub for Windows environment
-. (Resolve-Path "$env:LOCALAPPDATA\GitHub\shell.ps1")
-
-# Load PoshGit shell prompt
-. (Resolve-Path "$DF\powershell\posh-git\profile.example.ps1")
--- powershell/vs.ps1
+++
@@ -1,8 +0,0 @@
-pushd 'c:\Program Files (x86)\Microsoft Visual Studio 11.0\VC'
-cmd /c "vcvarsall.bat&set" |
-foreach {
-  if ($_ -match "=") {
-    $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
-  }
-}
-popd
