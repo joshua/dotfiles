7dd4fbdae2dc056ea973960dc6007b457b88a04e
joshs <josh@acst.com>
2017-11-21 10:36:26 -0500

    remove grc alias for make

--- zsh/custom/plugins/grc/grc.plugin.zsh
+++ zsh/custom/plugins/grc/grc.plugin.zsh
@@ -1,4 +1,8 @@
 # GRC colorizes nifty unix tools all over the place
 if $(grc &>/dev/null) && ! $(brew &>/dev/null); then
   source $HOMEBREW_ROOT/etc/grc.bashrc
+
+  # grc causes issues with interactive prompts.
+  # (docker -it, pprof, etc)
+  unalias make
 fi
