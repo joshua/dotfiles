0cc0f3de36176def12bc855b575ff7d3c2e0d1dd
joshs <josh@acst.com>
2017-11-21 10:31:18 -0500

    add zsh-autosuggestions

---
+++ zsh/custom/zsh-autosuggestions.zsh
@@ -0,0 +1,6 @@
+# Color to use when highlighting suggestion
+# Uses format of `region_highlight`
+# More info: http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Zle-Widgets
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=4,bold'
+
+bindkey '^ ' autosuggest-execute
