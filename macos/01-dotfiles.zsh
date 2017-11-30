50d32aa0b7490ba5dbb4504f712e8bb2d6d87eb8
joshs <josh@acst.com>
2017-11-30 12:51:48 -0500

    suppress warnings from chsh

--- macos/01-dotfiles.zsh
+++ macos/01-dotfiles.zsh
@@ -8,7 +8,7 @@ df::install
 log::step "Changing shell to ZSH for $USER"
 local shell=$(dscl . -read /Users/$USER UserShell | awk '{ print $2}')
 if [[ ! "$shell" = "zsh" ]]; then
-  sudo chsh -s zsh $USER
+  sudo chsh -s zsh $USER 2>/dev/null
 else
   log::g "shell already set to zsh for user $USER"
 fi
