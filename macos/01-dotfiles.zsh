1ab82e0f091b4af54b3d6ca8d139b59adbc01e6a
joshs <josh@acst.com>
2017-11-30 12:51:29 -0500

    only change shell if it's not already changed

--- macos/01-dotfiles.zsh
+++ macos/01-dotfiles.zsh
@@ -6,4 +6,9 @@ local DF=$root
 df::install
 
 log::step "Changing shell to ZSH for $USER"
-sudo chsh -s zsh $USER
+local shell=$(dscl . -read /Users/$USER UserShell | awk '{ print $2}')
+if [[ ! "$shell" = "zsh" ]]; then
+  sudo chsh -s zsh $USER
+else
+  log::g "shell already set to zsh for user $USER"
+fi
