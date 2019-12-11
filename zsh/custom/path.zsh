0bc753126244445b1d0931d7d40564a6ade97227
joshua <josh@acst.com>
2019-12-11 10:59:23 -0500

    add Couchbase bin to PATH

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -2,3 +2,5 @@ export PATH=$DF/bin:$HOME/bin:/usr/local/bin:$PATH
 
 # Postgres.app binaries
 export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.4/bin
+
+export PATH="$PATH:/Applications/Couchbase\ Server.app/Contents/Resources/couchbase-core/bin"
